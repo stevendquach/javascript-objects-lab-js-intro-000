f78f7788aea5630afe90a50817b9ff6b45280b42
Steven Quach <stevendquach@gmail.com>
2020-08-28 11:25:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var recipes = {turkey: 'gravy', beef: 'burger'}
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, { [key]: value })
 }
- destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-   return Object.assign({}, {key: 'value'}) 
+ destructivelyUpdateObjectWithKeyAndValue(recipes, turkey, gravy) {
+   return Object.assign({}, {turkey: 'cheery'}) 
 }
